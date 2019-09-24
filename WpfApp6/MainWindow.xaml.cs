15fb410d4cc8a84c6186756fa03add99598e40ba
sad <ds@pw.pl>
2019-09-24 15:09:56 +0200

    To be continued..

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string result = "";
+            Echo e1 = new Echo();
+            Echo e2 = new Echo();
+            int x = 0;
+            while (x < 4)
+            {
+                result = result + e1.Hello() + "\n";
+                e1.count = e1.count + 1;
+
+                if (x == 3)
+                {
+                    e2.count = e2.count + e1.count;
+                }
+                x = x + 1;
+            }
+            MessageBox.Show(result + "Licznik: " + e2.count);
+        }
+    }
+            public class Echo
+             {
+            public int count = 0;
+            public string Hello()
+            {
+            return "Czzeeeeść";
+            }
+    }
+}
+    
+
